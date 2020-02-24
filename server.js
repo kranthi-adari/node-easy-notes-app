47e4d7eda5ea787e4a717d774c0ef066046ab810
kranthi-adari <61175885+kranthi-adari@users.noreply.github.com>
2020-02-24 20:23:01 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//create express app
+const app = express();
+
+//parse request of content type -- application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}))
+
+//parse request of content type - application/json
+app.use(bodyParser.json())
+
+// Congfiguring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+
+//Connecting to the database 
+mongoose.connect(dbCongi.url, {
+useNewUrlParser: true
+})
+
+// define a sample route
+app.get('/', (req, res)=>{ res.json({"message": "Krad, Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."}); 
+});
+
+// listen for requests
+app.listen(3000, ()=> { 
+
+    console.log("Server is listening on port 3000")
+});
+
